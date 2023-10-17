3dc50156253e2a44fc80981a27c323a7d8b13745
Moath-Ali <azsexc71@gmail.com>
2023-10-17 23:52:36 +0300

    added all ejs files, one can add more later in the views file

--- js/express_server.js
+++ js/express_server.js
@@ -3,12 +3,50 @@ const app = express();
 const path = require("path")
 
 app.set("view engine","ejs")
-
-
 app.set("views",path.join(__dirname.substring(0,__dirname.length-2),"views"))
+app.use(express.static(__dirname.substring(0,__dirname.length-2) + ''));
+
+
 
 app.get("/",(req,res)=>{
-    res.render("webDev",{datas:"data"})
+    res.render("index",{datas:"data"})
+})
+
+app.get("/modules",(req,res)=>{
+    res.render("modules",{datas:"data"})
+})
+
+app.get("/modules/:path",(req,res)=>{
+    res.render("path",{datas:"data"})
+})
+
+app.get("/quiz",(req,res)=>{
+    res.render("quiz",{datas:"data"})
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login",{datas:"data"})
+})
+
+app.get("/register",(req,res)=>{
+    res.render("register",{datas:"data"})
+})
+
+app.get("/about-us",(req,res)=>{
+    res.render("about-us",{datas:"data"})
+})
+
+app.get("/forum",(req,res)=>{
+    res.render("forum",{datas:"data"})
+})
+
+app.get("/stories",(req,res)=>{
+    res.render("stories",{datas:"data"})
+    
+})
+
+app.get("*/",(req,res)=>{
+    res.render("not-found",{datas:"data"})
 })
 
 
