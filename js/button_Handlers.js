a8c6ea08aa2e8be5e4195eb1222ad9ec03b6fcd2
Moath-Ali <azsexc71@gmail.com>
2023-11-01 20:56:28 +0300

    z

--- js/button_Handlers.js
+++ js/button_Handlers.js
@@ -1,16 +1,10 @@
-
-
-// <textarea id="article_Text" cols="30" rows="10"></textarea>
+const tArea = document.getElementById("textArea");
 const editButton = document.getElementById("edit")
 const pName = document.querySelector("h1").innerText
-editButton.addEventListener("click",(e)=>{
-    const tArea = document.createElement("textarea")
-    tArea.id = "textArea"
-    tArea.cols = 50
-    tArea.rows = 50
-    tArea.style.display = "block"
-    
+const saveButton = document.getElementById("saveButton")
 
+
+editButton.addEventListener("click",(e)=>{
     fetch(`http://localhost:3000/getPath?name=${pName}`)
     .then(res=>{
         return res.json()
@@ -18,19 +12,10 @@ editButton.addEventListener("click",(e)=>{
     .then(data=>{
         tArea.innerText = data.article;
     })
-    document.body.appendChild(tArea)
-
-
-    const saveButton = document.createElement("button")
-    saveButton.innerText = "Save";
-    saveButton.addEventListener("click",sFunction)
-
-    document.body.appendChild(saveButton)
 })
 
+saveButton.addEventListener("click",sFunction)
 function sFunction(){
-    const tArea = document.getElementById("textArea")
-
     fetch(`http://localhost:3000/postPath?name=${pName}`,{
         method:"PUT",
         headers: {
