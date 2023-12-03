01c8cf2db852ee2b4706680fcf343b7708167fa7
Moath-Ali <azsexc71@gmail.com>
2023-12-03 20:37:57 +0300

    bcryptjs

--- js/express_server_login.js
+++ js/express_server_login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dbController = require('./dbController');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 var cookieParser = require('cookie-parser')
 router.use(cookieParser())
