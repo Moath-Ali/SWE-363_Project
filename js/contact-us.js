0572ed0c08e1859a391eb1a51fe3eeea2fd58bb9
TURKI-ALSUMARI <124775193+TURKI-ALSUMARI@users.noreply.github.com>
2023-12-05 20:48:18 +0300

    contact us done

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,4 +1,4 @@
- async function sendMail(){
+ function sendMail(){
   let parms = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
@@ -6,13 +6,17 @@
     message: document.getElementById("message").value
   }
   // if(parms.email !== "" && parms.name !== "" &&parms.subject !== ""&&parms.message !== ""){
-    try{
-      await emailjs.send("service_wohqpst","template_a9uq6vv",parms)
+    
+    emailjs.send("service_wohqpst","template_a9uq6vv",parms).then(function(res){
+      console.log("Sent successfully:", res);
+      window.location.href = "http://localhost:3000/contact-us-thank-you"
+    }, function(error) {
+      console.log("Failed to send:", error);
+      // Handle errors or show a message to the user
     }
-    catch(error){
-      console.error(error);
-      alert('Failed to send email');
-    }
+    )
+    
+    
   
   // }
   
